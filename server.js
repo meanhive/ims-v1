a5434dbdf276ebe0da3bd532caee96172d6c21d7
meanhive4 <meanhive4@gmail.com>
2022-09-07 22:09:01 +0530

    changes made in server in fourth commit of IMS-V1

--- server.js
+++ server.js
@@ -32,12 +32,14 @@ app.use(`/api/v1/auth`, authRoute)
 app.use(`/api/v1/image`, imageRoute)
 app.use(`/api/v1/product`, productRoute)
 
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-    app.use(express.static(`client/build`));
-    app.get(`*`, (req, res) => {
-        res.sendFile(path.join(__dirname + `/client/build/index.html`))
-    })
-}
+// if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+
+// }
+
+app.use(express.static(`client/build`));
+app.get(`*`, (req, res) => {
+    res.sendFile(path.join(__dirname + `/client/build/index.html`))
+})
 
 const PORT = process.env.PORT || 7000;
 const connectDB = require('./db')
