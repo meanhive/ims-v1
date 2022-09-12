5c1ad14ffad8d389e7a67cfd34682e3c052637af
meanhive <meanhive4@gmail.com>
2022-09-12 16:36:18 +0530

    updated caurosel image

--- client/src/images/index.js
+++ client/src/images/index.js
@@ -1,8 +1,8 @@
 const images = {
-    slide_1: "https://picsum.photos/id/341/1200/800",
-    slide_2: "https://picsum.photos/id/357/1200/800",
-    slide_3: "https://picsum.photos/id/366/1200/800",
-    slide_4: "https://picsum.photos/id/342/1200/800",
+    slide_1: "https://i.pinimg.com/originals/cf/a3/c8/cfa3c8018e52d4c88065406796df3231.jpg",
+    slide_2: "https://www.keg.com/hubfs/shutterstock_404189197.jpg",
+    slide_3: "https://img.freepik.com/free-vector/digital-online-education-concept-blank-space-laptop_255625-422.jpg?w=2000",
+    slide_4: "https://leverageedublog.s3.ap-south-1.amazonaws.com/blog/wp-content/uploads/2020/03/24185535/Online-Learning.jpg",
 }
 
 
